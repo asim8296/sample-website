4e5003f0f42bdb5a38fcf9e07826b86ffab72764
Asim Hashmi <hashmiasim2235@gmail.com>
2024-08-06 00:29:14 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById("changeTextButton").addEventListener("click", function() {
+    const paragraph = document.querySelector("main p");
+    paragraph.textContent = "The text has been changed!";
+});
+
